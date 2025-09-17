0d0249db173d726a34c32822d2bfa87ed791bf7d
annurtasnimislam <annurtasnimislam@gmail.com>
2025-09-17 20:00:32 +0600

    feat: optimal hashmap solution for moveZero

---
+++ solution_hashmap.js
@@ -0,0 +1,26 @@
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+function majorityElement(nums) {
+  let hashmap = {};
+
+  for (let num of nums) {
+    if (num in hashmap) {
+      hashmap[num] += 1;
+    } else {
+      hashmap[num] = 1;
+    }
+  }
+
+  for (let i in hashmap) {
+    if (hashmap[i] > nums.length / 2) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(majorityElement([3, 2, 3])); // 3
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2])); // 2
+console.log(majorityElement([3, 5, 5, 4, 5])); // 5
+console.log(majorityElement([3, 5, 4, 5])); // -1
